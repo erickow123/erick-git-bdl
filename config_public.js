5536f3368696371dddf2962800e99701eddbe1ec
erickow123 <49897659+erickow123@users.noreply.github.com>
2019-11-17 15:05:01 +0700

    Update config_public.js

--- config_public.js
+++ config_public.js
@@ -4,4 +4,4 @@ var ipServerTourism="http://gissurya.org/tourism_bkt/";*/
 
 var ipServerAngkot="http://13.76.103.93/erick-git-bdl/angkot_bkt/";
 var ipServerHotel="http://13.76.103.93/erick-git-bdl/hotel_bkt/";
-var ipServerTourism="http://localhost/html/tourism_bkt/";
+var ipServerTourism="http://13.76.103.93/erick-git-bdl/tourism_bkt/";
